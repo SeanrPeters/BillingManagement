fc4a3f8bf44286b5fe68292d9f82de0c41579f3c
SeanrPeters <58159948+SeanrPeters@users.noreply.github.com>
2023-08-13 21:24:59 -0400

    Create DropdownTrigger.jsx

---
+++ src/components/DropdownTrigger /DropdownTrigger.jsx
@@ -0,0 +1,24 @@
+
+import PropTypes from "prop-types";
+import React from "react";
+import { Down2 } from "../../icons/Down2";
+import "./style.css";
+
+export const DropdownTrigger = ({
+  colored,
+  className,
+  text = "Hover me",
+  icon = <Down2 className="down-1" color="#1890FF" />,
+}) => {
+  return (
+    <div className={`dropdown-trigger ${className}`}>
+      <div className={`title-4 ${colored}`}>{text}</div>
+      <div className="icon-wrapper">{icon}</div>
+    </div>
+  );
+};
+
+DropdownTrigger.propTypes = {
+  colored: PropTypes.oneOf(["off", "on"]),
+  text: PropTypes.string,
+};
